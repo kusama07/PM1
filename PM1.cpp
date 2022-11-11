d9a63972d87e55fb5f66be3ae4dd4fdd3a44960e
kusama07 <115055012+kusama07@users.noreply.github.com>
2022-11-11 10:57:53 +0900

    リモートリポジトリでコンフリクト変更

--- PM1.cpp
+++ PM1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello PM1 World!\n";
 
-    std::cout << "New branch\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
